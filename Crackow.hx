f158b58426e1de3624b23b560c565b03a6c5131a
Awsomiihill <awsomiihill@gmail.com>
2022-02-19 21:33:21 +0100

    a a  W A A  RF3JGVB RGFD

---
+++ Crackow.hx
@@ -0,0 +1,11 @@
+class Crackow {
+    static function main () {
+    var meow = "ur mama jeans";
+    var five = 2+2;
+        trace("o! OOO! !O! !O !O!O ICOdsan voi svioudfhv ouirfsjduov odfanvskosdfjvioewfb");
+        trace(meow, five);
+
+    }
+
+
+}
